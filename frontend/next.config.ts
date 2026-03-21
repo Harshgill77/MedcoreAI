aaa99f254df0d2bae3d3e5f086a1694c4fdaa5fe
HARSH <harsh@HARSHs-MacBook-Air.local>
2026-03-21 12:13:51 +0530

    fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   serverExternalPackages: ["@prisma/client", "prisma"],
   outputFileTracingIncludes: {
     "/**/*": ["./node_modules/.prisma/client/**/*"],
